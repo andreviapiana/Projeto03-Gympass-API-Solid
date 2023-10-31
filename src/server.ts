29af77fb013c0072fde82bc4cabf4205a5ff52a8
andreviapiana <andreviapiana@gmail.com>
2023-10-31 10:04:44 -0300

    ✨feat: Ajustes Iniciais: Variáveis de Ambiente + Zod

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log('🚀 HTTP Server Running!')
+    console.log(`🚀 HTTP Server Running at port ${env.PORT}`)
   })
