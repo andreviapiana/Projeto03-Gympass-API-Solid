1a444feb862182fd17e8be7f7e3b211ced76adb6
andreviapiana <andreviapiana@gmail.com>
2023-11-01 14:58:22 -0300

    ✨feat: Caso de Uso e Design Patterns - Lidando com Erros dos Casos de Uso (Use Case)

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -16,7 +17,7 @@ export class RegisterUseCase {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('E-mail already exists.')
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({
