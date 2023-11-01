fd742498ce735b53a17b4612de8b1055de5e81e3
andreviapiana <andreviapiana@gmail.com>
2023-11-01 15:04:19 -0300

    ✨feat: Caso de Uso e Design Patterns - Handler de Erros Global

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
 import fastify from 'fastify'
 import { appRoutes } from '@/http/routes'
+import { ZodError } from 'zod'
+import { env } from '@/env'
 
 export const app = fastify()
 
 // Rota de Criação de Usuários //
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
