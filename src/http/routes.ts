264e8e94eb041a69ccdcad64bfe8d844d9bcf67e
andreviapiana <andreviapiana@gmail.com>
2023-11-03 10:00:12 -0300

    ✨feat: Implementando Casos de Uso - Testes e Controller de Autenticação

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
+import { authenticate } from './controllers/authenticate'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+
+  app.post('/sessions', authenticate)
 }
