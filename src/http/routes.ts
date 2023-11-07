28f1a71c81dbcd8230aa2792b570711e32397a5f
andreviapiana <andreviapiana@gmail.com>
2023-11-07 14:46:47 -0300

    ✨feat: Controllers e Testes E2E - Controller de Perfil

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
+import { verifyJwt } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
@@ -9,5 +10,5 @@ export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticate)
 
   /** Authenticated */
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
