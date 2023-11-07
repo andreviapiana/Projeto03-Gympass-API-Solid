49dfff16772f6df4d5d080689b283412a1bb78cc
andreviapiana <andreviapiana@gmail.com>
2023-11-07 10:03:42 -0300

    ✨feat: Controllers e Testes E2E - Repositório de Academias

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,48 @@
+import { prisma } from '@/lib/prisma'
+import { Gym, Prisma } from '@prisma/client'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym
+  }
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    // Haversine Formula - Calcula a Distância entre 2 pontos no globo multiplicando o resultado por 6371 (raio do planeta) //
+    // A query SQL apresentada está realizando uma busca de todas as academias que estão a uma distância máxima de 10km da localização representada pela latitude e longitude informadas como parâmetros. //
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+}
