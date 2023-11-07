a364e7fc9d97f256551d9a9453f59c757dce88e5
andreviapiana <andreviapiana@gmail.com>
2023-11-07 10:23:39 -0300

    ✨feat: Controllers e Testes E2E - Princípios da Autenticação JWT – Controller de Perfil

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', profile)
 }
