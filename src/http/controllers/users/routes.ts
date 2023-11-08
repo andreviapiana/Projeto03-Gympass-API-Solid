77a2c3231f33cfae9a948bc7fe6f3dd37858488e
andreviapiana <andreviapiana@gmail.com>
2023-11-08 08:51:53 -0300

    ✨feat: Refresh Token & RBAC - Implementação do Refresh Token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,12 +2,16 @@ import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
+import { refresh } from './refresh'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  // Refresh Token //
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJwt] }, profile)
